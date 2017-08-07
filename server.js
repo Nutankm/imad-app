24f87236fe63c27182cc4818be7ac4eb7f2b7f79
Nutan Kumar <30651444+Nutankm@users.noreply.github.com>
2017-08-07 21:59:14 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,92 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne ={
+    title: 'Article One | Nutan Kumar',
+    heading: 'Article One',
+    date: 'Aug 07 2017',
+    content: `   <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                This is the content for my first article. This is the content for my first article. This is the content for my first article
+                This is the content for my first article. This is the content for my first article.
+            
+                </p>
+                
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                This is the content for my first article. This is the content for my first article. This is the content for my first article
+                This is the content for my first article. This is the content for my first article.
+            
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                This is the content for my first article. This is the content for my first article. This is the content for my first article
+                This is the content for my first article. This is the content for my first article.
+            
+                </p>  `
+};
+
+
+function createTemplate(data){
+    
+var title=data.title;
+var date=data.date;
+var content=data.content;
+var heading=data.heading;
+var htmlTemplate=`
+
+<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+            <meta name="viewport" content="width=device-width, intial-scale=1"/>
+             <link href="/ui/style.css" rel="stylesheet" />
+           
+            
+    </head>
+    <Body>
+            <div class="container">
+                
+                <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+               </div> 
+               </div>
+    </Body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+   res.send(createTemplate(articleTwo));
 });
 
 
 app.get('/article-three',function(req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+   res.send(createTemplate(articleThree));
 });
 
 
